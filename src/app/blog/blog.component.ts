577f0201742d8e77279ef6a2a847fda985b1c24d
Andrzej Czechowski <czechowski.priv@gmail.com>
2017-04-06 23:02:11 +0200

    Blog and about me in progress

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'sm-blog',    
     templateUrl: 'app/blog/blog.component.html',
-    styleUrls: ['app/blog/blog.component.css']       
 })
 
 export class BlogComponent {
