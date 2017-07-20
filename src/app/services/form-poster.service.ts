30b7b55ab80e7048824aa12278c83136674ecd14
Andrzej Czechowski <czechowski.priv@gmail.com>
2017-07-20 21:39:54 +0200

    Project after tuning rxjs requests, skipLibCheck in tsconfig.json (because of "typescript": "^2.4.1" errors)

--- src/app/services/form-poster.service.ts
+++ src/app/services/form-poster.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
 
 import { ContactEnvelope } from '../models/contactEnvelope.model';
 
